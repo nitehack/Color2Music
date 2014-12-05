9d393f931c6176b808f2d770d0ad8ee5c05aa4ce
nitehack <nitehack93@gmail.com>
2014-12-05 02:26:04 +0100

    'Sound'

--- source code/sound.c
+++ source code/sound.c
@@ -1,15 +1,20 @@
 
+#define SIn 126
+#define LABn 133
+#define LAn 141
+#define SOLBn 150
+#define SOLn 159
+#define FABn 168
+#define FAn 178
+#define MIn 189
+#define REBn 200
+#define REn 212
+#define DOBn 225
+#define DOn 238
 
-void genera_sonido(int8 frecuencia){
-   int8 fos;
-   int8 Pre=16;
-   int32 PR2;
-   float duty=0.5;
-   int8 frecuencia;
-   int8 value;
-   PR2=fos/(frecuencia*4*Pre)-1; //se esteblece el PR" segun la frecuencia seleccionada
-   value=duty*(4*(PR2+1));
-   
-   setup_timer_2(T2_DIV_BY_16, PR2_timer2 ,4); 
+void genera_sonido(int8 sonido){
+   int16 value;
+   value=(4*((int16)sonido+1))/2;
+   setup_timer_2(T2_DIV_BY_16, sonido ,4); 
    set_pwm1_duty(value); //se establece el ciclo de trabajo
 }
